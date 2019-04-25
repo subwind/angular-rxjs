221d587a2c34ea619553492901d65f9888cc7bbd
subwind <subwind@gmail.com>
2019-04-25 15:10:10 +0800

    fix app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ObservableComponent } from './View/Rxjs/Observable/observable.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:[ BrowserModule, FormsModule,AppRoutingModule],
+  declarations:[ AppComponent,HelloComponent, ObservableComponent],
+  bootstrap:[ AppComponent],
+  providers:[{ provide: APP_BASE_HREF, useValue : '/' }]
 })
 export class AppModule { }

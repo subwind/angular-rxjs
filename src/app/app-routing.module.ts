0609a8939832123fff306d3e48a1573a82132326
subwind <subwind@gmail.com>
2019-04-25 15:11:29 +0800

    add app-routing.module.ts

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ObservableComponent } from './View/Rxjs/Observable/observable.component'
+
+const routes: Routes = [
+  { path: 'observable', component: ObservableComponent },
+  { path: '', redirectTo: '', pathMatch: 'full'}
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+
+})
+export class AppRoutingModule { }
